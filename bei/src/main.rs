0be76c96b43c4d96b958f3a7d5ad92e1ef59e275
marc2332 <mespinsanz@gmail.com>
2023-07-28 11:27:32 +0200

    tweaks

--- bei/src/main.rs
+++ bei/src/main.rs
@@ -27,7 +27,11 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
     let mut mapper = unsafe { memory::init(phys_mem_offset) };
     let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
-    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
+    if allocator::init_heap(&mut mapper, &mut frame_allocator).is_ok() {
+        println!("[Success] Started the Heap.")
+    } else {
+        println!("[Error] Failed starting the Heap.")
+    }
 
     #[cfg(test)]
     test_bei();
